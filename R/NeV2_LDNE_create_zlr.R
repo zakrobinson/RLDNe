638e495896d45f958e388e5ea106f930bfe3412b
Zak Robinson <Zak@Zaks-MacBook-Air.local>
2019-01-21 18:14:55 -0700

    first commit

---
+++ R/NeV2_LDNE_create_zlr.R
@@ -0,0 +1,52 @@
+#'Write LDNe Parameter File
+#'
+#' This function writes a parameter for LD Method in NeEstmator V2.1
+#'
+#' @param input_file Genepop format file containing genotypic data
+#' @param param_file Desired name of parameter file produced
+#' @param Ne_out_file Desired name of output from LDNe
+#' @param matingsystem 0: Random mating, 1: Monogamy (LD method)
+#' @param crit_vals minimum allele frequency cutoff. Defaults to c(0.02,0.05,0.1).
+#' @author Zak Robinson, Contact: zachary.robinson(at)umontana.com
+#' @return Path of created LDNe param file
+#' @export
+
+
+
+
+NeV2_LDNe_create<-function(input_file, param_file,NE_out_file, matingsystem=1,crit_vals= c(0.02,0.05,0.1)){
+
+
+  ncrits<-length(crit_vals) #ncrits number of critical values
+  method=1 #Method 1: LD method; refer to manual for more methods
+  sink(file = param_file)
+  cat(paste(as.character(method),"\t* LD Method"))
+  cat("\n")
+  cat(paste(as.character(ncrits),"\t* number of critical values"))
+  cat("\n")
+  cat(paste(as.character(crit_vals)))
+  cat("\t* critical allele frequency values")
+  cat("\n")
+  cat("0\t*tabular output")
+  cat("\n")
+  cat("1\t* confidence intervals")
+  cat("\n")
+  cat(paste(as.character(matingsystem),"\t* 0: Random mating, 1: Monogamy (LD method)"))
+  cat("\n")
+  cat("0\t* max individual to be processed per pop, 0 for no limit")
+  cat("\n")
+  cat("0\t* Pop. range to run, given in pair. No limit if the first = 0")
+  cat("\n")
+  cat("0\t* Loc. ranges to run, given in pairs. No limit if the 1st = 0")
+  cat("\n")
+  cat(paste0(NE_out_file,"\t* output file name"))
+  cat("\n")
+  cat(paste(input_file,"\t* input file"))
+  cat("\n*")
+
+  sink()
+  return(param_file)
+
+}
+
+
---
+++ R/write_genepop_zlr_aw3.R
@@ -0,0 +1,128 @@
+#'Write Genepop file
+#'
+#' This function writes a genepop file from hierfstat format data.
+#'
+#' @param loci data.frame of genotypes in hierfstat format
+#' @param ind.ids vector of individual Identifiers ( must be equal in length to rows of genotypes)
+#' @param pops vector of (equal length to rows of genotypes) designating populations for each individual
+#' @param folder dir for saving genepop output file
+#' @param filepath file name for output file also the description at top of genepop file
+#' @param missingVal specifies the value of missing data in loci object
+#' @param ncode 2 x number of characters per allele (i.e. 22 = ncode = 2, since 1 character per allele going into the function * 2)
+#' @param diploid logical for whether the data is diploid or not
+#' @author Zak Robinson, Contact: zachary.robinson(at)umontana.com
+#' @return returns a list of length 2. The output contains the filepath of created genepop file and information for identifying populations following genepop analysis.
+#' @examples
+#' data("writegenepop_examp")
+#' write_genepop_zlr(loci = wgp_example[,3:ncol(wgp_example)],pops = wgp_example$pop,ind.ids = wgp_example$ind_id,folder = "",filepath ="genepop_output.txt",missingVal = NA,ncode = 2,diploid = T)
+#' @export
+#'
+#'
+write_genepop_zlr<-function(loci,pops,ind.ids,folder,filepath,missingVal=NA,ncode=2,diploid=T){
+
+## Note: if ncode is 2 and diploid is true then the alleles will be recoded to 2 digits
+
+
+  if(length(unlist(strsplit(filepath,split = "\\s",perl = T)))>1){
+    warning("genepop doesn't like spaces in file names")
+  }
+
+
+  d2_snp_fix<-function(string,missingval=missingVal){
+    if(is.na(missingval)){
+      if(!is.na(string)){
+        ns<-paste0("0",substring(text = string,1,1),"0",substring(text = string,2,2))
+        return(ns)
+      }
+      else{return(string)}
+    }
+    else{
+      if(string!=missingval){
+        ns<-paste0("0",substring(text = string,1,1),"0",substring(text = string,2,2))
+        return(ns)
+      }
+      else{return(string)}
+    }
+  }
+
+  if(ncode==2 & diploid==T){
+
+    loci<-apply(loci,MARGIN = c(1,2),FUN = d2_snp_fix)
+    ncode<-4
+  }
+
+
+
+  recode_missing_gp<-function(x,ncod=ncode, missingval=missingVal){
+    if(!is.na(missingval)){
+      if(x==missingval){
+        x<-paste0(rep(0,times=ncod),collapse = "")
+      }
+    }
+
+    if(is.na(x)){
+      x<-paste0(rep(0,times=ncod),collapse = "")
+    }
+    return(x)
+  }
+
+  bacon<-function(z,fin=lastid){# applied across rows to produce genotypes
+    if(z["pops"]==pop_levels[i]){
+      cat(paste(z["ind.ids"],","," "))
+      cat(paste(z[colnames(loci)]))
+      if(z["ind.ids"]==fin){
+        cat("  ")
+      }
+      else(cat("\n"))
+    }
+  }
+
+
+
+  loci<-as.data.frame(apply(X = loci,MARGIN = c(1,2),FUN = recode_missing_gp))
+
+
+
+    pop_levels<-unique(pops)
+
+    #####open file connection
+    sink(file = paste0(folder,filepath),append = F)
+    # write header of file
+    cat(filepath,sep = "\n")
+    x<-paste("Locus",1:ncol(loci),sep = "-")
+    y<-vector()
+    for(i in 1:length(x)){
+      if(i<length(x)){
+        y[i]<-paste0(x[i],","," ")
+      }
+    }
+    x<-c(y,x[length(x)])
+    cat(x,sep ="")
+    cat("\n")
+
+
+
+    ######write individual.ids and genotypes#######
+
+    x<-cbind(pops,ind.ids,loci)
+    lastid<-x[nrow(x),"ind.ids"]
+    popdecode<-data.frame(popname=rep(NA,length(pop_levels)),gp_popname=rep(NA,length(pop_levels)))
+    for(i in 1:length(pop_levels)){
+      cat("Pop\n")
+      apply(X = x,MARGIN=1,FUN =bacon)
+
+      popdecode$popname[i]<-as.character(pop_levels[i])
+      tmp<-as.character(x[which(x$pops==pop_levels[i]),"ind.ids"])
+      popdecode$gp_popname[i]<-tmp[length(tmp)]
+    }
+
+    sink()
+
+
+    output<-list(paste0(folder,filepath),popdecode)
+    names(output)<-c("Output_File","Pop_decode")
+    return(output)
+    closeAllConnections()
+  }
+
+#### end write genepop function
