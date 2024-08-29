52a3adcf88c54d830e61f86ce2d320c26842fca7
zrobinson <zrobinson@critfc.org>
2024-08-29 11:08:46 -0600

    minor updates to alleles2genotypes and genotypes2alleles

--- R/alleles2genotypes.R
+++ R/alleles2genotypes.R
@@ -6,9 +6,9 @@
 #'@param df Data frame that contains genotypic data and metadata
 #'@param alleles Numeric column positions or names of loci
 #'@param name_sep The locus-allele seperator in column names (e.g., "_" in Loc1_A1,Loc1_A2...LocN_A1,LocN_A2).
-#'@param name_pattern The locus-allele identifiers in column names specified by regex capture group (e.g., "(.+)_(A.$) performs the same as name_sep="_"). This is useful when the locus-allele separator string is repeated in column names (e.g, Loc_1_A1). This argument takes precedent over name_sep.
+#'@param name_pattern The locus-allele identifiers in column names specified by 2 regex capture groups (e.g., "(.+)_(A.$) performs the same as name_sep="_"). This is useful when the locus-allele separator string is repeated in column names (e.g, Loc_1_A1). This argument takes precedent over name_sep.
 #'@param suffix Logical that allele identifier is a suffix (e.g., Loc1_A1 instead of A1_Loc1). Default=T.
-#'@param Adelim Delimiter string between alleles. The default="" is not ideal when allele character lengths vary.
+#'@param Adelim Desired delimiter string between alleles in output. The default="" is not ideal when allele character lengths vary.
 #'@author Zak Robinson, Contact: <zrobinson@critfc.org>
 #'@return Data frame in genotype per column format
 #'@details This function is meant to conveniently wrap tidyr and dplyr operations to convert data between two common wide formats. At the moment it works for diploid data when there is a common locus-allele identifier. If the input varies (e.g., Loc1.a1 Loc2_A1) you will want to gsub your way to uniformity or write your own solution.
@@ -26,6 +26,9 @@ alleles2genotypes <- function(df,alleles,name_sep=NULL,name_pattern=NULL,suffix=
   if(is.null(name_pattern)&is.null(name_sep))stop("name_sep or name_pattern must be specified")
   if(!is.null(name_pattern)&!is.null(name_sep))warning("name_sep and name_pattern are both specified. name_pattern takes precedent")
   if(is.null(name_pattern)){
+    seps <- unlist(strsplit(name_sep,split = ""))
+    sepsEsc <- ifelse(seps %in% c(".", "*", "+", "?", "^", "$", "(", ")", "[", "]", "{", "}", "|"),paste0('\\',seps),seps)
+    name_sep <- paste(sepsEsc,collapse = "")
     name_pattern=paste0("(.+)",name_sep,"(.+)")
   }
 if(length(alleles)%%2!=0)stop("Odd number of alleles provided, we're dealing with diploid data here?")
--- R/genotypes2alleles.R
+++ R/genotypes2alleles.R
@@ -5,8 +5,9 @@
 #'
 #'@param df Data frame that contains genotypic data and metadata
 #'@param loci Numeric column positions or names of loci
-#'@param fixed_A The number of characters of alleles. A diploid genotype "AT" has a fixed_A=1. Only used when the delimiter between alleles is "".
-#'@param delim A non-zero length character string separating alleles. For example, ":" for "A:T" or "-" 114-116"
+#'@param fixed_A The number of characters of alleles in input. A diploid genotype "AT" has a fixed_A=1. Only use when the delimiter between alleles is "".
+#'@param delim A non-zero length character string separating alleles in input. For example, ":" for "A:T" or "-" 114-116"
+#'@param name_sep The desired locus allele name separator for output column names e.g., "_" for Loc1_A1. Defaults to "."
 #'@author Zak Robinson, Contact: <zrobinson@critfc.org>
 #'@return Data frame in allele per column format
 #'@details This function is meant to conveniently wrap tidyr and dplyr operations to convert data between two common wide formats. At present, it's just set up for diploid data.
@@ -20,14 +21,14 @@
 
 
 
-genotypes2alleles <-function(df,loci,fixed_A=NULL,delim=NULL){
+genotypes2alleles <-function(df,loci,fixed_A=NULL,delim=NULL,name_sep="."){
 
   if(is.null(delim)&is.null(fixed_A)) stop("fixed_A or delim must be specified")
   if(!is.null(delim)&!is.null(fixed_A)) message("delim takes precedent over fixed_A")
 
 
   if(!is.null(delim)){
-    NAtest <- df %>% select(loci) %>% reframe(across(everything(),~sum(grepl(delim,.x))/sum(!is.na(.x))))
+    NAtest <- df %>% select(all_of(loci)) %>% reframe(across(everything(),~sum(grepl(delim,.x))/sum(!is.na(.x))))
     if(any(NAtest<1)) message("Data suggests some non-missing genotypes do not contain delimiter")
     alleles_df <-  df %>%
       tidyr::pivot_longer(cols = loci,
@@ -53,7 +54,7 @@ genotypes2alleles <-function(df,loci,fixed_A=NULL,delim=NULL){
       tidyr::pivot_longer(cols = c(A1,A2),
                           names_to = "allele") %>%
       tidyr::pivot_wider(names_from = c(locus,allele),
-                         values_from = value,names_sort = F)
+                         values_from = value,names_sort = F,names_sep = name_sep)
   }
 
   return(alleles_df)
